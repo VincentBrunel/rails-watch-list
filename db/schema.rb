80b3b2cf6390709c60c8e3ebb385dce073304ed0
vincent brunel <vincent.brunel20@gmail.com>
2022-08-18 13:07:16 +0200

    models

--- db/schema.rb
+++ db/schema.rb
@@ -16,12 +16,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_18_094929) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.string "comment"
-    t.bigint "movie_id", null: false
-    t.bigint "list_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["list_id"], name: "index_bookmarks_on_list_id"
-    t.index ["movie_id"], name: "index_bookmarks_on_movie_id"
   end
 
   create_table "lists", force: :cascade do |t|
@@ -32,14 +28,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_18_094929) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
-    t.text "overview"
-    t.string "poster_url"
-    t.string "string"
-    t.integer "rating"
+    t.string "overview"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "bookmarks", "lists"
-  add_foreign_key "bookmarks", "movies"
 end
